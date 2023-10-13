13fa5e99101f9ed2f6e953e6b0fae59f82ac8aee
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-10-13 13:20:55 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header>
+        <h1>My 12 Col Grid</h1>
+        <nav>
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+        </nav>
+      </header>
+      <div className="welcome-section">
+        <img src="https://source.unsplash.com/random" alt="random img" />
+        <div className="welcome-blurb">
+          <h2>Welcome</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+            inventore? Unde quos est, odio, corrupti voluptatibus nesciunt et
+            incidunt sed alias sunt necessitatibus laudantium pariatur libero
+            maiores, veritatis minima sapiente? Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Beatae ut cupiditate nam sint quia
+            nobis nulla maxime amet inventore praesentium voluptatibus maiores
+            officia repellendus, rem id nesciunt sequi iste sit.
+          </p>
+        </div>
+      </div>
+      <h2>Services</h2>
+      <div className="container">
+        <h3>Hello</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, magni.
+          Exercitationem dolor distinctio reprehenderit labore, quas est impedit
+          alias laborum nemo, placeat, tempora officia architecto facere
+          accusamus ut vero repellendus.
+        </p>
+      </div>
+      <div className="container">
+        <h3>Hello</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, magni.
+          Exercitationem dolor distinctio reprehenderit labore, quas est impedit
+          alias laborum nemo, placeat, tempora officia architecto facere
+          accusamus ut vero repellendus.
+        </p>
+      </div>
+      <div className="container">
+        <h3>Hello</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, magni.
+          Exercitationem dolor distinctio reprehenderit labore, quas est impedit
+          alias laborum nemo, placeat, tempora officia architecto facere
+          accusamus ut vero repellendus.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
